177300daae95db1addb9174e85d15b91b337a18a
J2Z-22 <105422067+J2Z-22@users.noreply.github.com>
2022-05-12 17:17:43 +0800

    Add files via upload

---
+++ global.R
@@ -0,0 +1,19 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinydashboard)#主题
+library(shinyBS)#按钮
+library(shinythemes)
+library(shinyWidgets)#提示
+library(RLumShiny)#颜色
+library(tidyverse)
+library(smplot)
+library(grid)#下载
+library(gridExtra)
